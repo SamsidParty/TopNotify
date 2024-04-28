4fb63c56008a5a91d5f7c7743616841592455696
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-04-28 21:50:24 +0300

    Complete Migration To Vite

--- TopNotify.WWW/topnotify/src/main.jsx
+++ TopNotify.WWW/topnotify/src/main.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { NextUIProvider, createTheme } from '@nextui-org/react';
 import './index.css'
 
+window.serverURL = "http://" + window.location.host + "/";
+
+//For UWP
+if (navigator.gamepadInputEmulation) {
+    navigator.gamepadInputEmulation = "gamepad";
+}
+
+//Dev Mode
+if (window.location.hostname != "localhost") {
+    serverURL = "http://localhost:25631/";
+
+    //Interop Injection Stub
+    if (window.external.receiveMessage == undefined) {
+        window.chrome.webview.addEventListener('message', message => eval.call(window, message));
+        window.chrome.webview.postMessage(`{"Type":"load"}`);
+    }
+    else {
+        window.external.receiveMessage(message => eval.call(window, message));
+        window.external.sendMessage(`{"Type":"load"}`);
+    }
+}
+
+const lightTheme = createTheme({
+    type: 'light'
+})
+  
+const darkTheme = createTheme({
+    type: 'dark'
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <NextUIProvider theme={(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ? darkTheme : lightTheme}>
+        <App />
+    </NextUIProvider>,
 )
+
+//TODO: Come Up With A Better Solution Than This
+setTimeout(() => {
+    document.body.dispatchEvent(new Event("reactReady"));
+}, 250);
