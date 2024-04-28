25489ff6caeeff57160a7459e4aabec7f4683cff
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-04-28 21:50:24 +0300

    Complete Migration To Vite

--- TopNotify.WWW/topnotify/src/App.jsx
+++ TopNotify.WWW/topnotify/src/App.jsx
@@ -1,35 +1,97 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as NextUI from "@nextui-org/react"
+
+var Config = {
+    Location: -1,
+    RunOnStartup: true,
+    Opacity: 0
+}
+
+window.SetConfig = (e) => {
+    Config = JSON.parse(e);
+    window.setRerender(Math.random());
+}
+
+function UploadConfig() {
+
+    if (Config.Location == -1) {
+        //Config Hasn't Loaded Yet
+        return;
+    }
+
+    var ev = new CustomEvent("uploadConfig");
+    ev.newConfig = JSON.stringify(Config);
+    document.body.dispatchEvent(ev);
+    window.setRerender(Math.random());
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    var [rerender, setRerender] = useState(Math.random());
+    window.setRerender = setRerender;
+
+    
+    return (
+        <>
+            <h2>Settings</h2>
+
+            <NextUI.Spacer></NextUI.Spacer>
+
+            <div className="locationCard">
+                <div className="notifyLocation tl"><NextUI.Button onPress={() => ChangeLocation(0)} flat auto>{Config.Location == 0 ? "\uea5e" : "\ued27"}</NextUI.Button></div>
+                <div className="notifyLocation tr"><NextUI.Button onPress={() => ChangeLocation(1)} flat auto>{Config.Location == 1 ? "\uea5e" : "\ued27"}</NextUI.Button></div>
+                <div className="notifyLocation bl"><NextUI.Button onPress={() => ChangeLocation(2)} flat auto>{Config.Location == 2 ? "\uea5e" : "\ued27"}</NextUI.Button></div>
+                <div className="notifyLocation br"><NextUI.Button onPress={() => ChangeLocation(3)} flat auto>{Config.Location == 3 ? "\uea5e" : "\ued27"}</NextUI.Button></div>
+            </div>
+
+            <div className="divider"></div>
+
+            <div className="flexx facenter fillx gap20 buttonContainer">
+                <label>Spawn Test Notification</label>
+                <NextUI.Button css={{ marginLeft: "auto" }} className="iconButton" auto onPress={SpawnTestNotification}>
+                    &#xea99;
+                </NextUI.Button>
+            </div>
+
+            <div className="divider"></div>
+
+            <div className="flexx facenter fillx gap20">
+                <label>Run On Startup</label>
+                <NextUI.Switch onChange={(e) => ChangeSwitch("RunOnStartup", e)} checked={Config.RunOnStartup} css={{ marginLeft: "auto" }}>
+                    &#xea99;
+                </NextUI.Switch>
+            </div>
+
+            <div className="divider"></div>
+
+            <div className="flexy fillx gap20">
+                <label>Notification Opacity</label>
+                <NextUI.Pagination onChange={ChangeOpacity} page={Math.round(6 - Config.Opacity)} rounded onlyDots total={6} />
+            </div>
+        </>
+    )
+}
+
+function SpawnTestNotification() {
+    document.body.dispatchEvent(new Event("spawnTestNotification"));
+}
+
+function ChangeLocation(location) {
+    Config.Location = location;
+    UploadConfig();
+    window.setRerender(Math.random());
+}
+
+function ChangeOpacity(opacity) {
+    Config.Opacity = (6 - opacity);
+    UploadConfig();
+    window.setRerender(Math.random());
+}
+
+function ChangeSwitch(key, e) {
+    Config[key] = e.target.checked;
+    UploadConfig();
+    window.setRerender(Math.random());
 }
 
 export default App
