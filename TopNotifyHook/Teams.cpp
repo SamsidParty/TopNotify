e139a2d04626e4aaf5af2ba7f289d8b5da65332f
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-06-18 16:47:19 +0300

    Update Hook DLL Finding

--- TopNotifyHook/Teams.cpp
+++ TopNotifyHook/Teams.cpp
@@ -1,5 +1,6 @@
 #include "Teams.h"
 #include <easyhook.h>
+#include <mmsystem.h>
 
 void Teams::SetupHooks() {
 	HOOK_TRACE_INFO posHook = { NULL };
@@ -7,7 +8,7 @@ void Teams::SetupHooks() {
 	NTSTATUS result = LhInstallHook(GetProcAddress(GetModuleHandle(TEXT("user32")), "SetWindowPos"), SetWindowPosHook, NULL, &posHook);
 
 	if (!NT_SUCCESS(result)) {
-		//Display Error MEssage
+		//Display Error Message
 		MessageBoxW(NULL, L"Failed To Setup Hooks For Microsoft Teams, TopNotify Won't Intercept Teams Notifications", L"TopNotify", 0);
 		return;
 	}
@@ -21,6 +22,7 @@ BOOL Teams::SetWindowPosHook(HWND hWnd, HWND hWndInsertAfter, int x, int y, int 
 	//372PX Seems To Be The Standard Width Of Teams Notifications
 	//So Check If It's 372PX To Determine Whether This Window Is A Notification Window
 	if (cx == 372) {
+		//PlaySoundW(L"C:\\Users\\SamarthCat\\Documents\\Programming Stuff\\TopNotify\\TopNotify.WWW\\topnotify\\public\\Audio\\fortnite\\levelup_c5s1.wav", NULL, SND_FILENAME | SND_ASYNC);
 		return SetWindowPos(hWnd, hWndInsertAfter, 15, 15, cx, cy, uFlags);
 	}
 	else {
