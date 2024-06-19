eda7293300fcd17fbf2f4bd72dd317bd76360e6f
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-06-19 06:15:06 +0300

    Setup IPC Infrastructure

---
+++ TopNotify/Daemon/IPCServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace TopNotify.Daemon
+{
+    public class IPCServer
+    {
+        private WebSocketServer SocketServer;
+
+
+        public IPCServer()
+        {
+            SocketServer = new WebSocketServer("ws://127.0.0.1:"); // TODO: Assign A Port
+        }
+
+        public void Start()
+        {
+
+        }
+    }
+}
--- TopNotifyHook/Teams.h
+++ TopNotifyHook/Teams.h
@@ -4,8 +4,8 @@
 
 class Teams
 {
-public:
-	static void SetupHooks();
-	static BOOL SetWindowPosHook(HWND hWnd, HWND hWndInsertAfter, int x, int y, int cx, int cy, UINT uFlags);
+	public:
+		static void SetupHooks();
+		static BOOL SetWindowPosHook(HWND hWnd, HWND hWndInsertAfter, int x, int y, int cx, int cy, UINT uFlags);
 };
 
