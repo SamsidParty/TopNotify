96c5b855a5f66779d32342f74622127686030ce2
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-06-19 08:55:29 +0300

    Setup WebSocket Server

--- TopNotify/Daemon/IPCServer.cs
+++ TopNotify/Daemon/IPCServer.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WebSocketSharp;
 using WebSocketSharp.Server;
+using Logger = WebFramework.Backend.Logger;
 
 namespace TopNotify.Daemon
 {
@@ -17,12 +18,36 @@ namespace TopNotify.Daemon
 
         public IPCServer()
         {
-            SocketServer = new WebSocketServer("ws://127.0.0.1:"); // TODO: Assign A Port
+            SocketServer = new WebSocketServer("ws://127.0.0.1:27631");
+            SocketServer.AddWebSocketService<IPCHandler>("/ipc");
         }
 
         public void Start()
         {
+            if (SocketServer != null)
+            {
+                Logger.LogInfo("Starting Daemon WebSocket Server");
+                SocketServer.Start();
+            }
+        }
 
+        public void Stop()
+        {
+            if (SocketServer != null)
+            {
+                Logger.LogInfo("Stopping Daemon WebSocket Server");
+                SocketServer.Stop();
+            }
         }
     }
+
+    public class IPCHandler : WebSocketBehavior 
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Logger.LogInfo("Got Message: " + e.Data);
+            base.OnMessage(e);
+        }
+    }
+
 }
