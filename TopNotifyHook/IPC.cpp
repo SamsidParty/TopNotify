721f7b0337ea093004596d7e1a20c9d09251e16b
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-06-22 19:04:02 +0300

    Add JSON Parsing In Hook

--- TopNotifyHook/IPC.cpp
+++ TopNotifyHook/IPC.cpp
@@ -1,21 +1,26 @@
 #include "ipc.h"
+#define BOOST_JSON_STACK_BUFFER_SIZE 1024
+#include <boost/json.hpp>
+#include <boost/json/src.hpp>
 
+
+using namespace boost::asio;
 namespace beast = boost::beast;
 namespace http = beast::http;
 namespace websocket = beast::websocket;
-namespace net = boost::asio;
 using tcp = boost::asio::ip::tcp;
+using namespace boost::json;
 
 void IPC::RunIPC() {
     try
     {
-        net::io_context ioc;
+        io_context ioc;
 
         tcp::resolver resolver { ioc };
         websocket::stream<tcp::socket> ws { ioc };
 
         auto results = resolver.resolve(std::string("127.0.0.1"), "27631");
-        auto ep = net::connect(ws.next_layer(), results);
+        auto ep = connect(ws.next_layer(), results);
 
         std::string host = "127.0.0.1:" + std::to_string(27631);
 
@@ -25,10 +30,17 @@ void IPC::RunIPC() {
 
         //Send Request For Config File
         const char packet = 0;
-        ws.write(net::buffer(&packet, 1));
+        ws.write(boost::asio::buffer(&packet, 1));
 
         while (ws.read(buffer)) {
-            MessageBox(NULL, std::to_wstring(buffer.size()).c_str(), L"Got Data", 0);
+
+            try {
+                std::string json = beast::buffers_to_string(buffer.data() + 1);
+                value settingsFile = parse(json);
+                MessageBoxA(NULL, settingsFile.at("SoundPath").as_string().c_str(), "Got Data", 0);
+            }
+            catch (...) { }
+
             buffer.clear();
         }
 
@@ -37,7 +49,7 @@ void IPC::RunIPC() {
         MessageBox(NULL, L"Disconnected From TopNotify Daemon", L"TopNotify Hook", 0);
         IPC::RunIPC();
     }
-    catch (std::exception const& e)
+    catch (...)
     {
         MessageBox(NULL, L"Failed To Connect To TopNotify Daemon", L"TopNotify Hook", 0);
         IPC::RunIPC();
