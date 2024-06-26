19b5ff93b5b3355f8bae03d64f62b1eb9f63ca70
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-06-26 15:20:53 +0300

    Use A More Reliable Method To Determine Ready

---
+++ TopNotify.WWW/topnotify/src/Helper.jsx
@@ -0,0 +1,20 @@
+import { useRef } from "react";
+
+//https://stackoverflow.com/a/66139558/18071273
+export function useFirstRender() {
+    const ref = useRef(true);
+    const firstRender = ref.current;
+    ref.current = false;
+    return firstRender;
+}
+
+//https://stackoverflow.com/a/52652681/18071273
+export function waitUntil(conditionFunction) {
+
+    const poll = resolve => {
+        if(conditionFunction()) resolve();
+        else setTimeout(_ => poll(resolve), 400);
+    }
+  
+    return new Promise(poll);
+}
