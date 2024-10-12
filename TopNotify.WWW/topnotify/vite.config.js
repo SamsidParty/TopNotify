2f3c688614bfc875da6ad708268da1186e80353f
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-10-12 10:29:36 +0300

    Replace Version Number At Build Time

--- TopNotify.WWW/topnotify/vite.config.js
+++ TopNotify.WWW/topnotify/vite.config.js
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import transformPlugin from 'vite-plugin-transform';
+import { resolve, join } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '..\\..\\TopNotify\\WWW'
-  }
+    plugins: [
+        react(),
+        transformPlugin({
+            tStart: '%{',
+            tEnd: '}%',
+            replace: {
+                "TOPNOTIFY_VERSION": "2.4.6"
+            },
+            replaceFiles: [
+                resolve(join(__dirname, '..\\..\\TopNotify\\WWW\\Meta\\AppxManifest.xml')),
+                resolve(join(__dirname, '..\\..\\TopNotify\\WWW\\Meta\\manifest.xml')),
+                resolve(join(__dirname, '..\\..\\TopNotify\\WWW\\Meta\\WinGet\\SamsidParty.TopNotifyWG.yaml')),
+                resolve(join(__dirname, '..\\..\\TopNotify\\WWW\\Meta\\WinGet\\SamsidParty.TopNotifyWG.installer.yaml')),
+                resolve(join(__dirname, '..\\..\\TopNotify\\WWW\\Meta\\WinGet\\SamsidParty.TopNotifyWG.locale.en-US.yaml')),
+            ]
+        })
+    ],
+    build: {
+        outDir: '..\\..\\TopNotify\\WWW'
+    }
 })
