be068ef553bac3ef585ecfaa2293e57da62d3a5b
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-01-01 13:28:02 +0300

    V3 UI: Boilerplate

--- TopNotify.Tauri/src-tauri/src/lib.rs
+++ TopNotify.Tauri/src-tauri/src/lib.rs
@@ -1,14 +1,19 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::Manager;
+use window_vibrancy::*;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+
+            #[cfg(target_os = "windows")]
+            apply_mica(&window, None)
+                .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("Couldn't start TopNotify! Make sure the WebView2 runtime is installed.");
 }
---
+++ TopNotify.Tauri/src/Pages/SplashScreen.jsx
@@ -0,0 +1,12 @@
+import "../CSS/SplashScreen.css";
+
+export default function SplashScreen() {
+    return (
+        <div className="splashScreen">
+            <div className="splashInnerBox">
+                <img alt="TopNotify Icon" src="/Image/Icon.png"></img>
+                <h1>TopNotify</h1>
+            </div>
+        </div>
+    )
+}
