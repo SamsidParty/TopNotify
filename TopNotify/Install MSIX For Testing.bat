d73218894b5028661e097bd585863ec677b0e6c8
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-02-26 15:48:16 +0300

    Chore: Bump System.Speech to version 10
    Closes #49
    
    Co-Authored-By: SamsidParty <business@samsidparty.com>

--- TopNotify/Install MSIX For Testing.bat
+++ TopNotify/Install MSIX For Testing.bat
@@ -4,7 +4,7 @@ SET F=".\BUILD"
 IF EXIST %F% RMDIR /S /Q %F%
 
 c:\windows\system32\xcopy.exe .\MSIX .\BUILD /E /H /C /I
-c:\windows\system32\xcopy.exe /s .\bin\Release\net8.0-windows10.0.17763.0 .\BUILD
+c:\windows\system32\xcopy.exe /s .\bin\Release\net9.0-windows10.0.17763.0 .\BUILD
 
 cd .\BUILD
 
